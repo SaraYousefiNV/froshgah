e1f36a1805ab6e0877aa8b5ca4082e5a8e997a50
SaraYousefiNV <sarayousefi_krc@yahoo.com>
2023-05-30 18:52:53 +0330

    changeproduct

--- ConsoleStore/DataModels/Product.cs
+++ ConsoleStore/DataModels/Product.cs
@@ -9,7 +9,7 @@ namespace ConsoleStore.DataModels
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public double Price { get; set; }
         public int CategoryId { get; set; }
         public override string ToString()
